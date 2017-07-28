d38abe9212ed951cb97201ed9dfee5164d0d01d2
Shannon Noe <shannon.noe@flightaware.com>
2017-07-28 19:16:43 +0000

    Add comment about initialization function

--- Sources/demoext.swift
+++ Sources/demoext.swift
@@ -7,13 +7,28 @@
 import Tcl8_6
 import SwiftTcl
 
+// A trivial function to export to tcl
 func demoext (interp: TclInterp, objv: [TclObj]) -> String {
 	return "demoext"
 }
 
+// What's this silgen_name thing?
+//
+// When tcl loads a platform library with the load command, tcl
+// looks for an initialization function in the dynamic library.
+//
+// This C linkage name is dependent on the name of the .so file
+// If the tcl code is
+//   load "./libSwiftTclDemoExt.so"
+// then the C initialization entrypoint has to be
+//   filename - "lib" and ".so"
+//   all lowercase
+//   captialize first character
+
 @_silgen_name("Swifttcldemoext_Init")
 func Demoext_Init(_ interpPtr:UnsafeMutablePointer<Tcl_Interp>) -> Int {
 	let interp = TclInterp(interp: interpPtr)	
+	// Add the command to tcl
     interp.createCommand(named: "demoext", using: demoext)
     return 0
 }
